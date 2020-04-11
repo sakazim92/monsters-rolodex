6daa9c8bd0b3d7bd65f54b6c36097977318abee0
ali.kazim <ali.kazim@timesaco.com>
2020-04-11 22:24:34 +0500

    Updates

---
+++ precache-manifest.3d799b123a3165085fd6d3cc281c58c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bd734be4e7c500c728be3de6a66a8a9",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "981c3e1fdb8fb52adf1c",
+    "url": "/monsters-rolodex/static/css/main.8b502b91.chunk.css"
+  },
+  {
+    "revision": "147f1327e7798733efc4",
+    "url": "/monsters-rolodex/static/js/2.d562cfe4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.d562cfe4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "981c3e1fdb8fb52adf1c",
+    "url": "/monsters-rolodex/static/js/main.fc28bda7.chunk.js"
+  },
+  {
+    "revision": "83fef09b6b4b91e8e383",
+    "url": "/monsters-rolodex/static/js/runtime-main.6036d2a0.js"
+  }
+]);
